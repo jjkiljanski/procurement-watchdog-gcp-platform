06b91339951f6ae81d95a20af9132104ad793331
Jan Kiljanski <jan.jozef.kiljanski@gmail.com>
2026-05-07 00:10:03 +0200

    Allow workflows to run downloader jobs
    Grant the Cloud Workflows orchestrator service account roles/run.invoker so bzp-backfill can call Cloud Run Jobs run on the bzp-downloader job. This fixes the run.jobs.run permission denied error seen when triggering range backfills.

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -102,6 +102,12 @@ resource "google_project_iam_member" "orchestrator_run_developer" {
   member  = "serviceAccount:${google_service_account.orchestrator.email}"
 }
 
+resource "google_project_iam_member" "orchestrator_run_invoker" {
+  project = var.project_id
+  role    = "roles/run.invoker"
+  member  = "serviceAccount:${google_service_account.orchestrator.email}"
+}
+
 resource "google_project_iam_member" "orchestrator_dataproc_editor" {
   project = var.project_id
   role    = "roles/dataproc.editor"
